a4f431c0fd510d1343629f5bce791d2ce37b41fb
prajjwal0217 <mahajan1702@gmail.com>
2024-03-25 21:27:14 +0530

    flatbuyer bugs fixed

--- FlatBuyerHandOn/FlatBuyer/src/main/java/com/example/demo/bean/Buyer.java
+++ FlatBuyerHandOn/FlatBuyer/src/main/java/com/example/demo/bean/Buyer.java
@@ -3,10 +3,10 @@ package com.example.demo.bean;
 import java.util.List;
 import java.util.ArrayList;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Buyer {
